ff806fc20f1a777f3d27e08ebe91c6d36abb9426
Daniele Viti <dnviti@outlook.com>
2021-06-20 02:02:42 +0200

    -

--- Data/using.cs
+++ Data/using.cs
@@ -10,5 +10,6 @@ global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
